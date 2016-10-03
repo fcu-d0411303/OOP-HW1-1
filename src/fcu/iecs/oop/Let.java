3af8bea321bc9b52d51caadffa3eba04ab9b67b6
User <User@DESKTOP-M9BMA8N>
2016-10-03 22:32:38 +0900

    第一周作業

---
+++ src/fcu/iecs/oop/Let.java
@@ -0,0 +1,13 @@
+package fcu.iecs.oop;
+
+public class Let {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String lyrics ="Let it go!Let it go! Cannot hoid it back anymore";
+		String lyrics2 = lyrics.replace("it","her");
+		String lyrics3 = lyrics2.replace("Cannot","Can't");
+		System.out.println(lyrics3);
+	}
+
+}
